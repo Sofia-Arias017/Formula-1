cde777c087fce1070f5653560685b521d4efed39
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-05-05 04:37:03 -0500

    feat: :sparkles: Se agrego buscar nombre del vehiculo

--- src/js/vehiculos.js
+++ src/js/vehiculos.js
@@ -1,22 +1,37 @@
-import { vehiculos } from '../data/vehiculosData.js'; 
+import { vehiculos } from '../data/vehiculosData.js';
+import '../components/vehiculo-card.js'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     const contenedor = document.getElementById('vehiculos-contenedor');
-
+    const inputBusqueda = document.getElementById('busqueda');
 
     if (!contenedor) {
-    console.error('No se encontró el contenedor con id "vehiculos-container"');
-    return;
-}
+        console.error('No se encontró el contenedor con id "vehiculos-contenedor"');
+        return;
+    }
 
     if (!vehiculos || vehiculos.length === 0) {
-    console.warn('No hay datos de vehículos para mostrar.');
-    return;
-}
+        console.warn('No hay datos de vehículos para mostrar.');
+        return;
+    }
 
-    vehiculos.forEach(vehiculo => {
-    const card = document.createElement('vehiculo-card');
-    card.vehiculo = vehiculo;
-    contenedor.appendChild(card);
-});
+    function mostrarVehiculos(lista) {
+        contenedor.innerHTML = '';
+        lista.forEach(vehiculo => {
+            const card = document.createElement('vehiculo-card');
+            card.vehiculo = vehiculo;
+            contenedor.appendChild(card);
+        });
+    }
+
+    mostrarVehiculos(vehiculos);
+
+    inputBusqueda.addEventListener('input', () => {
+        const termino = inputBusqueda.value.toLowerCase();
+        const filtrados = vehiculos.filter(v =>
+            v.nombre.toLowerCase().includes(termino) ||
+            v.equipo.toLowerCase().includes(termino)
+        );
+        mostrarVehiculos(filtrados);
+    });
 });
